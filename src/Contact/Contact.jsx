cbe798dab198c20ffb41c93262430dc973880d2e
Miriam Mwongela <133253468+miriammwongela1@users.noreply.github.com>
2026-01-20 23:43:55 +0300

    Cange

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -24,7 +24,7 @@ export default function Contact() {
   console.log("Submit button clicked!");
 
   try {
-    const response = await fetch("http://localhost:5000/send-email", {
+    const response = await fetch("/.netlify/functions/send-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
