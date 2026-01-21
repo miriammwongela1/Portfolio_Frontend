bc298cf79eaf91087a2eadf895f89155f5e39ac4
Miriam Mwongela <133253468+miriammwongela1@users.noreply.github.com>
2026-01-21 19:56:58 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header/Header.jsx';
 import About from './About/About.jsx';
-import Skills from './Skills/Skills.jsx';
 import Projects from './Projects/Projects.jsx';
 import Contact from './Contact/Contact.jsx';
 import Footer from './Footer/Footer.jsx';
@@ -22,7 +21,7 @@ function HomePage() {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Personal_Portfolio">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/swms" element={<><Header /><SWMS /><Footer /></>} />
