080d58c55ad0445d0bcbc573cb7b229310599eac
Miriam Mwongela <133253468+miriammwongela1@users.noreply.github.com>
2026-01-21 20:13:39 +0300

    another

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './variable.css'
 import App from './App.jsx'
@@ -7,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Personal_Portfolio">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
